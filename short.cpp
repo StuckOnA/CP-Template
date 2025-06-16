26cb9103c7de0879b6c660596349f8e82f589cf3
StuckOnA <shahadatjarif008@gmail.com>
2025-06-16 15:57:56 +0600

    Create short.cpp

---
+++ short.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define db(x) cout << #x << " = " << x << endl;
+
+using namespace std;
+
+const int INF = 1'000'000'000'000'000'000;
+const int MOD = 1'000'000'000 + 7;
+
+void solve(int tc) {
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    for (int c = 1; c <= t; c++) {
+        solve(c);
+    }
+
+    return 0;
+}
+
